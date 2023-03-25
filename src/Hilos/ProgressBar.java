d17d61e0611a0461c52d383484bdc2b5b9f457d1
JRafaelLobo <jose.lobo@unitec.edu>
2023-03-24 18:40:08 -0600

    Otro Comentario por Seguridad

--- src/Hilos/ProgressBar.java
+++ src/Hilos/ProgressBar.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hilos;
 
-/**
- *
- * @author Rafael
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JProgressBar;
+
 public class ProgressBar {
-    
+
+    javax.swing.JProgressBar ProgressBar;
+    int SegundosInicio, SegundosFinal;
+    boolean Vivo = true;
+    int torneos;
+
+    public ProgressBar(JProgressBar ProgressBar, int SegundosInicio, int SegundosFinal) {
+        this.ProgressBar = ProgressBar;
+        this.SegundosInicio = SegundosInicio * 1000;
+        this.SegundosFinal = SegundosFinal * 1000;
+    }
+
+    public ProgressBar(JProgressBar ProgressBar, int torneos) {
+        this.ProgressBar = ProgressBar;
+        this.torneos = torneos;
+        this.SegundosFinal = this.torneos * 7 * 1000;
+        this.SegundosInicio = 0;
+    }
+
+    public void run() {
+        ProgressBar.setMaximum(SegundosFinal);
+        while (Vivo) {
+            while (SegundosInicio < SegundosFinal) {
+                ProgressBar.setValue(SegundosInicio);
+                SegundosInicio += 100;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+            if (SegundosFinal >= SegundosInicio) {
+                Vivo = false;
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
